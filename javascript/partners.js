d2815ea709c2e7dacf0a6fb53bfc9347406130ea
Robert Lavender <robjklavender@hotmail.co.uk>
2025-11-03 15:37:33 +0000

    added code

--- javascript/partners.js
+++ javascript/partners.js
@@ -1,16 +1,22 @@
-id = 1
-
-setInterval(function(){
-    $('#box' + id).animate({
-        left: '-50%'
-    }, 500, function() {
-        $(this).css('left', '150%');
-        $(this).appendTo('#container');
+$(document).ready(function() {   
+    $('.partner-box').slick({
+        infinite: true,
+        arrows: false,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3500,
+        speed: 300,
+        variableWidth: true,
+        draggable: false,
+        centerMode: false,
+        responsive: [ // so it fixes bug on it disappearing on desktop view (desktop first)
+        {
+        breakpoint: 768, 
+        settings: {
+            centerMode: true,
+        }
+        }]
     });
+});
 
-    $('#box' + id).next().animate({
-        left: '50%'
-    }, 500);
-
-    id = id <= 5 ? id + 1 : 1;
-},4000)
