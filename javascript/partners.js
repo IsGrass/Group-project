c33b377400a8eb7b7e229f94e3bd9974ee842edf
Robert Lavender <robjklavender@hotmail.co.uk>
2025-11-07 12:40:52 +0000

    updated the css

--- javascript/partners.js
+++ javascript/partners.js
@@ -1,3 +1,6 @@
+//used slick to make the imgs move across the screen 
+// used autoplay option with arrows and draggable turned off
+//it scrolls them idivuyally
 $(document).ready(function() {   
     $('.partner-box').slick({
         infinite: true,
@@ -5,18 +8,19 @@ $(document).ready(function() {
         slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3500,
+        autoplaySpeed: 3000,
         speed: 300,
         variableWidth: true,
         draggable: false,
         centerMode: false,
-        responsive: [ // so it fixes bug on it disappearing on desktop view (desktop first)
-        {
-        breakpoint: 768, 
-        settings: {
-            centerMode: true,
-        }
-        }]
+        responsive: [ // so it fixes bug on it disappearing on screenj sizes above 768
+            {
+                breakpoint: 768, 
+                settings: {
+                    centerMode: true,
+                }
+            }
+        ]
     });
 });
 
