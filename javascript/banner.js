6c3adb423d6976b45385c9005d9bbf4288d866e6
Robert Lavender <robjklavender@hotmail.co.uk>
2025-11-10 13:49:44 +0000

    made working banner

---
+++ javascript/banner.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {   
+    $('.sliding-image').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4300,
+        speed: 300,
+    });
+});
