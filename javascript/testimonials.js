0ca9407f7ad679d1a0e179a879a7ee223bc31450
IsGrass <Dennisboldyjr@gmail.com>
2025-11-10 13:38:25 +0000

    testimonials

---
+++ javascript/testimonials.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {   
+    $('.review-box1').slick({
+        infinite: true,
+        arrows: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        variableWidth: true,
+        draggable: false,
+        centerMode: false,
+        responsive: [ // so it fixes bug on it disappearing on screenj sizes above 768
+            {
+                breakpoint: 768, 
+                settings: {
+                    centerMode: true,
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function() {   
+    $('.review-box2').slick({
+        infinite: true,
+        arrows: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        variableWidth: true,
+        draggable: false,
+        centerMode: false,
+        responsive: [ // so it fixes bug on it disappearing on screenj sizes above 768
+            {
+                breakpoint: 768, 
+                settings: {
+                    centerMode: true,
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function() {   
+    $('.review-box3').slick({
+        infinite: true,
+        arrows: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        variableWidth: true,
+        draggable: false,
+        centerMode: false,
+        responsive: [ // so it fixes bug on it disappearing on screenj sizes above 768
+            {
+                breakpoint: 768, 
+                settings: {
+                    centerMode: true,
+                }
+            }
+        ]
+    });
+});
