bb08c6e471b464fd24268d00783de9d5de9cd923
Robert Lavender <robjklavender@hotmail.co.uk>
2025-11-17 10:28:33 +0000

    fixed margins and icons postioning

--- javascript/testimonials.js
+++ javascript/testimonials.js
@@ -2,13 +2,14 @@ $(document).ready(function() {
     $('.review-box1').slick({
         infinite: true,
         arrows: false,
-        slidesToShow: 5,
+        slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
         speed: 1000,
+        dots: true,
         variableWidth: true,
-        draggable: false,
+        draggable: true,
         centerMode: false,
         responsive: [ // so it fixes bug on it disappearing on screenj sizes above 768
             {
@@ -25,13 +26,14 @@ $(document).ready(function() {
     $('.review-box2').slick({
         infinite: true,
         arrows: false,
-        slidesToShow: 5,
+        slidesToShow: 1,
         slidesToScroll: 1,
+        dots: true,
         autoplay: true,
         autoplaySpeed: 3000,
         speed: 1000,
         variableWidth: true,
-        draggable: false,
+        draggable: true,
         centerMode: false,
         responsive: [ // so it fixes bug on it disappearing on screenj sizes above 768
             {
@@ -48,13 +50,14 @@ $(document).ready(function() {
     $('.review-box3').slick({
         infinite: true,
         arrows: false,
-        slidesToShow: 5,
+        slidesToShow: 1,
         slidesToScroll: 1,
+        dots: true,
         autoplay: true,
         autoplaySpeed: 3000,
         speed: 1000,
         variableWidth: true,
-        draggable: false,
+        draggable: true,
         centerMode: false,
         responsive: [ // so it fixes bug on it disappearing on screenj sizes above 768
             {
