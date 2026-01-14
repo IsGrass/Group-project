59a11b3265ebd0303453c8d201b7b935aedcf715
Robert Lavender <robjklavender@hotmail.co.uk>
2026-01-14 14:26:01 +0000

    updated sliders and side menu

--- javascript/testimonials.js
+++ javascript/testimonials.js
@@ -1,35 +1,19 @@
 //All of the 3 testimonials banners javascript
 
 //I decided to use slick jquery plugin to the banner on the testimonials page.
-//These banner have arrow buttons to slide through the different slides and is draggable on mobile.
-//It only shows one slide at a time out of a total of 3 per banner with each of them having their own background image and content text.
+//It only shows one slide at a time.
 //These banners are for the reveiews from different jobs we have contducted such as corporate,weddings and events we have conducted.
-//On smaller screen sizes the arrows disapear and are replaced with touchable dots since the arrows clip at the sides of the phone.
 //This banner does not auto play and requires the user to scroll through them.
 $(document).ready(function() {   
     $('.review-box').slick({
         infinite: true,
-        arrows: true,
-        dots: false,
+        arrows: false,
+        dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: false,
         speed: 300,
         draggable: true,
-        responsive: [
-      {
-        breakpoint: 808,
-        settings: {
-            arrows: false,
-            dots:true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-        }
-        
-      }
-      
-    ]
-
     });
 });
 
